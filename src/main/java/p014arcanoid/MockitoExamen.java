82095b5d04526e4cabfbd53f8ebc294d32b3cdcb
JoTerrance <gorka.zgz@gmail.com>
2019-05-17 13:29:29 +0200

    test

--- src/main/java/p014arcanoid/MockitoExamen.java
+++ src/main/java/p014arcanoid/MockitoExamen.java
@@ -1,15 +1,14 @@
 package p014arcanoid;
 
-import java.io.InputStream;
-
 import org.omg.CORBA.TypeCodePackage.BadKind;
+import org.omg.CORBA.portable.InputStream;
 import org.omg.CosNaming.NamingContextExtPackage.AddressHelper;
 
 public class MockitoExamen {
 
 	public void mockitoEj1(AddressHelper helper, InputStream stream) throws BadKind {
 		
-		helper.read((org.omg.CORBA.portable.InputStream) stream);
+		helper.read(stream);
 		if(helper.type().content_type().default_index()==0) {
 			return;
 		}else {
--- src/test/java/p014arcanoid/MockitoExamenTest.java
+++ src/test/java/p014arcanoid/MockitoExamenTest.java
@@ -1,19 +1,20 @@
 package p014arcanoid;
 
-import java.io.InputStream;
-
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.omg.CORBA.TypeCodePackage.BadKind;
+import org.omg.CORBA.portable.InputStream;
 import org.omg.CosNaming.NamingContextExtPackage.AddressHelper;
 
 class MockitoExamenTest {
-
+	@Mock
+	AddressHelper helper;
 
 	@Test
 	void test() throws BadKind {
 		MockitoExamen examen = new MockitoExamen();
-		InputStream stream = null;
-		AddressHelper helper = null;
+		InputStream stream = Mockito.mock(InputStream.class);
 		examen.mockitoEj1(helper, stream);
 	}
 
